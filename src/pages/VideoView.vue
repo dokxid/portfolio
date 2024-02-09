b6f6396adfc3aee71aca46cbbda2368324c47454
dokxid <dan-du@live.de>
2024-02-09 02:05:53 +0100

    update .gitignore, add div to videoview, add parallel script for tailwind watch and dev

--- src/pages/VideoView.vue
+++ src/pages/VideoView.vue
@@ -4,10 +4,17 @@ import DiscographyVideoC from "../components/DiscographyVideoC.vue";
 
 <template>
   <!-- col layout -->
-  <div class="flex justify-center items-center grow">
+  <div class="flex flex-col justify-center items-center grow gap-4">
     <!-- hero content -->
-      <div class="">
-        <DiscographyVideoC />
+    <div class="">
+      <DiscographyVideoC />
+    </div>
+    <div>
+      <div class="flex max-w-screen-xl bg-neutral p-5 rounded-3xl items-center">
+        <article class="prose prose-slate my-6">
+          <h1 class="lead my-0">some of my projects</h1>
+        </article>
       </div>
+    </div>
   </div>
 </template>
